8bccadd2bbe8a54d40c7c741b97452534a951a16
Gnoq <emi.cugno@gmail.com>
2020-12-17 18:43:28 -0300

    WIP login view

---
+++ src/store/ui-params/getters.js
@@ -0,0 +1,20 @@
+export default {
+  isLoading(state) {
+    return state.displaySpinnerOverlay;
+  },
+  showNavBar(state) {
+    return state.showNavBar;
+  },
+  snackbar(state) {
+    return state.snackbar;
+  },
+  showSignInSupervisor(state) {
+    return state.showSignInSupervisor;
+  },
+  showSignInResource(state) {
+    return state.showSignInResource;
+  },
+  showSignInAdmin(state) {
+    return state.showSignInAdmin;
+  }
+};
---
+++ src/store/ui-params/state.js
@@ -0,0 +1,14 @@
+export default {
+  displaySpinnerOverlay: false,
+  errorDetected: false,
+  showNavBar: true,
+  showSignInSupervisor: false,
+  showSignInAdmin: false,
+  showSignInResource: false,
+  snackbar: {
+    visible: false,
+    color: "primary",
+    text: null,
+    timeout: 2000
+  }
+};
