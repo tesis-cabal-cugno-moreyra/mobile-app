b21cdb449b5ff7b78a5f6f820a5f4c06b3ca310e
Gnoq <emi.cugno@gmail.com>
2021-02-22 18:49:43 -0300

    Clean network services code, remove unnecesary one-line functions

--- src/services/networkServices.js
+++ src/services/networkServices.js
@@ -1,19 +1,14 @@
 import { Plugins } from '@capacitor/core';
 const { Network } = Plugins;
-//import store from '../store'
-
 
 export default {
   async listenNetworkStatus(vueContext) {
-    let initialStatus = await this.getInitialNetworkStatus();
+    let initialStatus = await Network.getStatus();
     vueContext.$store.dispatch("mobileEventsStatus/changeNetworkStatus", initialStatus.connectionType);
 
     Network.addListener("networkStatusChange",  status => {
       vueContext.$store.dispatch("mobileEventsStatus/changeNetworkStatus", status.connectionType);
       // If network is down and a websocket is open (an incident is open and current user is active in it), geolocation points must be stored locally.
     });
-  },
-  async getInitialNetworkStatus() {
-    return await Network.getStatus();
   }
 };
