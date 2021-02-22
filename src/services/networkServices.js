3ff896d50ed9b1c878dcf4c87e7d5d79da1bc9b1
Gnoq <emi.cugno@gmail.com>
2021-02-22 18:47:22 -0300

    Get initial network status, and then start to listen if it changes

--- src/services/networkServices.js
+++ src/services/networkServices.js
@@ -4,10 +4,16 @@ const { Network } = Plugins;
 
 
 export default {
-  listenNetworkStatus(vueContext) {
+  async listenNetworkStatus(vueContext) {
+    let initialStatus = await this.getInitialNetworkStatus();
+    vueContext.$store.dispatch("mobileEventsStatus/changeNetworkStatus", initialStatus.connectionType);
+
     Network.addListener("networkStatusChange",  status => {
       vueContext.$store.dispatch("mobileEventsStatus/changeNetworkStatus", status.connectionType);
       // If network is down and a websocket is open (an incident is open and current user is active in it), geolocation points must be stored locally.
     });
+  },
+  async getInitialNetworkStatus() {
+    return await Network.getStatus();
   }
 };
