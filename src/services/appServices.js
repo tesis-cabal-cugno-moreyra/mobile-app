8f8ea3fc1b470f3e9010711144b11d76da2f330f
Gnoq <emi.cugno@gmail.com>
2021-02-25 19:45:49 -0300

    Remove useless console log

--- src/services/appServices.js
+++ src/services/appServices.js
@@ -5,7 +5,6 @@ export default {
   listenAppStatus(vueContext) {
     App.addListener('appStateChange', (state) => {
       // state.isActive contains the active state
-      console.log(state);
       vueContext.$store.dispatch("mobileEventsStatus/changeApplicationStatus", state.isActive);
     });
   }
