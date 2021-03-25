f13d193cbdb636c967bec554c746a16479afb856
Gnoq <emi.cugno@gmail.com>
2021-03-25 19:07:51 -0300

    Need fix: Incident id has been hardcoded like a champ, someone must change it (maybe I will do later... maybe) :bug:

--- src/services/webSocketServices.js
+++ src/services/webSocketServices.js
@@ -3,7 +3,7 @@ import websocketAdapters from "@/adapters/websocketAdapters";
 export default {
   sendPoint(point) {
     let adaptedPoint = websocketAdapters.pointAdapter(point);
-    const ws = new WebSocket('wss://tesis-cabal-cugno-moreyra-back.herokuapp.com/ws/incident/1/');
+    const ws = new WebSocket('wss://tesis-cabal-cugno-moreyra-back.herokuapp.com/ws/incident/67/');
     ws.onopen = function () {
       ws.send(adaptedPoint);
       console.info("Success sending message through WebSocket!");
