58c21bd484b9a1e8342768205c1500cc391b1ca1
Facundo Moreyra <fmoreyra2011@gmail.com>
2021-03-28 19:51:28 -0300

    Fix 104, union de recursos completo, flujo de incidente en app

---
+++ src/components/ResourceAbleToContainResourceList.vue
@@ -0,0 +1,313 @@
+<template>
+  <v-form>
+    <v-row justify="center">
+      <v-dialog v-model="showResourceToContainResource.visible"
+                v-if="showResourceToContainResource.visible?  (this.loadData = showResourceToContainResource.resourceToContainResource)
+              : (this.loadData = null) "
+                persistent
+                width="600" dark>
+
+        <v-card>
+          <v-card-title :class="['pa-3', 'mt-7', 'black_selected']">
+            <v-row align="center" justify="center">
+              Vehículos relacionados
+            </v-row>
+          </v-card-title>
+
+          <v-card-text :class="[' black_selected', 'pa-1']">
+            <v-data-table
+                :loading="loadingTable"
+                loading-text="Cargando... Espere por favor"
+                :headers="headersResource"
+                :items="resourceData"
+                text-center
+                item-key="id"
+                :class="['pb-1']"
+                hide-default-footer
+            >
+              <template v-slot:top>
+                <v-dialog v-model="dialogChangeStatus" max-width="300px">
+                  <v-card>
+                    <v-card-title class="justify-center"
+                    ><p class="text-center">¿Desea unirse a </p>
+                      <p>este vehiculo?</p>
+                    </v-card-title
+                    >
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                          color="success"
+                          outlined
+                          @click="changeStateConfirmationMessage"
+                          :class="['mr-5']"
+                      >Acepto
+                      </v-btn
+                      >
+                      <v-btn
+                          color="primary"
+                          outlined
+                          @click="dialogChangeStatus = false"
+                      >Cancelar
+                      </v-btn
+                      >
+                      <v-spacer></v-spacer>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+                <v-dialog v-model="dialogDeleteStatus" max-width="300px">
+                  <v-card>
+                    <v-card-title class="justify-center"
+                    ><p>¿Desea salir de </p>
+                      <p>este vehiculo?</p>
+                    </v-card-title
+                    >
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                          color="success"
+                          outlined
+                          @click="changeStateConfirmationResourceRemove"
+                          :class="['mr-5']"
+                      >Acepto
+                      </v-btn
+                      >
+                      <v-btn
+                          color="primary"
+                          outlined
+                          @click="dialogDeleteStatus = false"
+                      >Cancelar
+                      </v-btn
+                      >
+                      <v-spacer></v-spacer>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </template>
+              <template v-slot:item.actions="{ item }">
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        v-show="item.resource.id !== showResourceToContainResource.idContainerResource "
+                        v-bind="attrs"
+                        v-on="on"
+                        small
+                        color="success"
+                        @click="openDialogPlus(item)"
+                    >
+                      <v-icon>
+                        mdi-account-plus
+                      </v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Ingresar al vehiculo</span>
+                </v-tooltip>
+
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        v-show="item.resource.id === showResourceToContainResource.idContainerResource"
+                        v-bind="attrs"
+                        v-on="on"
+                        small
+                        color="primary"
+                        @click="openDialogRemove(item)"
+                    >
+                      <v-icon>
+                        mdi-account-remove
+                      </v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Salir del vehiculo</span>
+                </v-tooltip>
+              </template>
+
+            </v-data-table>
+            <v-pagination
+                v-model="page"
+                class="my-4"
+                :total-visible="10"
+                :length="numberOfPage"
+            ></v-pagination>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="onClose()">Cerrar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </v-form>
+
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: "resourceAbleToContainResourceList",
+  data() {
+    return {
+      referenceSearch: {
+        user__first_name: "",
+        user__last_name: "",
+        type__name: "",
+        user__is_active: true,
+        page: 1
+      },
+      loadingTable: false,
+      loadingProcessInfo: false,
+      isComponentEnable: false,
+      dialogChangeStatus: false,
+      dialogDeleteStatus: false,
+      singleSelect: false,
+      searchName: "",
+      searchLastName: "",
+      autoCompleteTypeResource: "",
+      page: 1,
+      numberOfPage: 1,
+      selected: [],
+      typeResourceSelectedList: [],
+      toJoinResource: true,
+      nextPagination: null,
+      headersResource: [
+        {
+          text: "Nombre",
+          align: "start",
+          sortable: false,
+          value: "resource.user.username"
+        },
+        {text: "Tipo de recurso", sortable: false, value: "resource.type.name"},
+        {
+          text: "Unirse",
+          value: "actions",
+          sortable: false
+        }
+      ],
+      loadData: null,
+      resourceData: [],
+      container_resource_id: null
+    };
+  },
+  async mounted() {
+    if (this.showResourceToContainResource.visible === true) {
+      this.loadResourceData();
+    }
+  },
+  watch: {
+
+    page() {
+      this.loadResourceData();
+    },
+    loadData() {
+      this.loadResourceData();
+    }
+  },
+  methods: {
+    openDialogPlus(resourceData) {
+      this.dialogChangeStatus = true;
+      this.container_resource_id = resourceData.resource.id
+    },
+    openDialogRemove(resourceData) {
+      this.dialogDeleteStatus = true;
+      this.container_resource_id = resourceData.resource.id
+    },
+    async changeStateConfirmationResourceRemove() {
+      let neededInfo = {
+        incident_id: this.userInformation.incidentId,
+        resource_id: this.userInformation.resourceId,
+
+      }
+      await this.$store
+          .dispatch("incident/updateResource", neededInfo)
+          .then(() => {
+
+            this.$store.commit("uiParams/dispatchAlert", {
+              text: "Se desvinculo correctamente",
+              color: "success"
+            });
+            this.dialogDeleteStatus = false;
+            this.container_resource_id = null;
+            this.$store.commit(
+                "incident/changeVisibilityResourceToContainResource",
+                {
+                  resourceToContainResource: this.showResourceToContainResource.resourceToContainResource,
+                  idContainerResource: null
+                }
+            );
+
+          })
+          .catch(e => {
+            console.error(e);
+            this.$store.commit("uiParams/dispatchAlert", {
+              text: "Hubo un problema para desvincularse",
+              color: "primary"
+            });
+
+          })
+
+    },
+    async changeStateConfirmationMessage() {
+      let neededInfo = {
+        incident_id: this.userInformation.incidentId,
+        resource_id: this.userInformation.resourceId,
+        container_resource_id: this.container_resource_id
+
+      }
+      await this.$store
+          .dispatch("incident/updateResource", neededInfo)
+          .then(() => {
+            this.$store.commit("uiParams/dispatchAlert", {
+              text: "Se vinculo correctamente",
+              color: "success"
+            });
+            this.dialogChangeStatus = false;
+
+            this.$store.commit(
+                "incident/changeVisibilityResourceToContainResource",
+                {
+                  resourceToContainResource: this.showResourceToContainResource.resourceToContainResource,
+                  idContainerResource: this.container_resource_id
+                }
+            );
+            this.container_resource_id = null;
+          })
+          .catch(e => {
+            console.error(e);
+            this.$store.commit("uiParams/dispatchAlert", {
+              text: "Hubo un problema para vincularlo",
+              color: "primary"
+            });
+
+          })
+
+    },
+    onClose() {
+      this.$store.commit("incident/closeResourceToContainResource");
+    },
+
+    loadResourceData() {
+      this.loadingTable = true;
+      if (this.showResourceToContainResource.resourceToContainResource.count > 0) {
+        this.resourceData = this.showResourceToContainResource.resourceToContainResource.results;
+
+        let itemsPerPage = process.env.VUE_APP_ITEMS_PER_PAGE;
+        if (!itemsPerPage) {
+          console.error("Variable no declarada en: VUE_APP_ITEMS_PER_PAGE");
+        }
+
+        this.numberOfPage = Math.ceil(this.showResourceToContainResource.resourceToContainResource.count / itemsPerPage);
+      }
+      this.loadingTable = false;
+    },
+
+  },
+  computed: {
+    ...mapGetters({
+      showResourceToContainResource: "incident/showResourceToContainResource",
+      userInformation: "incident/incidentUserData"
+    })
+  }
+
+};
+
+</script>
--- src/views/OngoingIncident.vue
+++ src/views/OngoingIncident.vue
@@ -1,62 +1,204 @@
-
 <template>
-  <v-app>
+  <v-container>
     <nav-bar></nav-bar>
-      <v-main>
-        <v-container >
-          <v-btn
-              @click="leaveIncident()"
-              color="primary"
-          >Salir del incidente</v-btn
-          >
-        </v-container>
-      </v-main>
-  </v-app>
+    <v-row align="center" dense class="pt-4 mt-4">
+      <v-col cols="12">
+        <div class="text-center">
+          <div class="my-8">
+            <v-btn
+                min-width="300"
+                @click="viewLinkedVehicles()"
+                color="primary"
+                class="pa-5"
+            >Ver vehículos vinculados
+              <v-icon class="ml-3">
+                mdi-truck-check
+              </v-icon>
+            </v-btn>
+          </div>
+
+          <div class="my-8">
+            <v-btn
+                min-width="300"
+                @click="insertMapPoint()"
+                color="primary"
+                class="pa-5"
+            >Notificar situación
+              <v-icon class="ml-13">
+                mdi-cellphone-information
+              </v-icon>
+            </v-btn>
+          </div>
+
+          <div>
+            <v-btn
+                min-width="300"
+                @click="seeIncidentLocation()"
+                color="primary"
+                class="pa-5"
+            >Ver mapa del incidente
+              <v-icon class="ml-4">
+                mdi-map-marker
+              </v-icon>
+            </v-btn>
+          </div>
+          <div class="my-8">
+            <v-btn
+                min-width="300"
+                @click="leaveIncident()"
+                color="primary"
+                class="pa-5"
+            >Salir del incidente
+              <v-icon class="ml-13">
+                mdi-exit-to-app
+              </v-icon>
+            </v-btn>
+          </div>
+        </div>
+        <v-dialog v-model="dialogExitIncidentStatus" max-width="300px">
+          <v-card>
+            <v-card-title class="justify-center"
+            ><p>¿Desea salir de </p>
+              <p>este incidente?</p>
+            </v-card-title
+            >
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                  color="success"
+                  outlined
+                  @click="changeStateConfirmationExitToIncident"
+                  :class="['mr-5']"
+              >Salir
+              </v-btn
+              >
+              <v-btn
+                  color="primary"
+                  outlined
+                  @click="dialogExitIncidentStatus = false"
+              >Cancelar
+              </v-btn
+              >
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+    </v-row>
+    <resourceAbleToContainResourceList></resourceAbleToContainResourceList>
+  </v-container>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import NavBar from "@/components/NavBar";
+import {mapGetters} from "vuex";
+import resourceAbleToContainResourceList from "../components/ResourceAbleToContainResourceList.vue";
+import NavBar from "../components/NavBar";
 
 export default {
-name: "OngoingIncident",
-  components: {NavBar},
+  name: "OngoingIncident",
+  components: {NavBar, resourceAbleToContainResourceList},
+  data() {
+    return {
+      dialogExitIncidentStatus: false
+    }
+  },
   methods: {
-  async leaveIncident()
-  {
-    await this.$store
+    async leaveIncident() {
+      this.dialogExitIncidentStatus = true;
+    },
+    async viewLinkedVehicles() {
+      const dataResource = localStorage.getItem("user");
+      const resourceObject = JSON.parse(dataResource)
+      const username = resourceObject.username
+      let idContainerResource = null;
+
+      await this.$store
+          .dispatch("incident/getResourceVehicleIncident",
+              {
+                incident_id: this.resourceIdIncidentId.incidentId,
+                username_Resource: username,
+                resource_to_contain_resources: 'false',
+                has_container_resource: 'false'
+              })
+          .then(response => {
+            if (response.data.results[0].container_resource !== null) {
+              idContainerResource = response.data.results[0].container_resource.id;
+            }
+          })
+          .catch(e => {
+            console.error(e);
+          })
+
+      await this.$store
+          .dispatch("incident/getResourceVehicleIncident",
+              {
+                incident_id: this.resourceIdIncidentId.incidentId,
+                username_Resource: '',
+                resource_to_contain_resources: 'true'
+              })
+          .then(response => {
+            if (response.data.count === 0) {
+              this.$store.commit("uiParams/dispatchAlert", {
+                text: "De momento no hay vehiculos vinculados al incidente",
+                color: "primary",
+                timeout: 4000
+              });
+            } else {
+              this.$store.commit(
+                  "incident/changeVisibilityResourceToContainResource",
+                  {
+                    resourceToContainResource: response.data,
+                    idContainerResource: idContainerResource
+                  }
+              );
+            }
+
+
+          })
+          .catch(e => {
+            console.error(e);
+            this.$store.commit("uiParams/dispatchAlert", {
+              text: "Hubo un problema para encontrar vehiculos vinculados al incidente",
+              color: "primary",
+              timeout: 4000
+            });
+
+          })
+
+    },
+    insertMapPoint() {
+      console.log(this.$router)
+    },
+    seeIncidentLocation() {
+      console.log("Aqui iria un mapa de la incidencia")
+    },
+    async changeStateConfirmationExitToIncident()
+    { await this.$store
         .dispatch("incident/deleteResourceIncident", this.userInformation)
-        .then( () => {
-          this.$router.push({ name: "ActiveIncidents" });
+        .then(() => {
+          this.$router.push({name: "ActiveIncidents"});
 
           this.$store.commit("uiParams/dispatchAlert", {
             text: "Se lo quito del incidente correctamente",
-            color: "#49cc90",
+            color: "success",
             timeout: 4000
           });
 
         })
-        .catch(async () => {
-
+        .catch(e => {
+          console.error(e);
           this.$store.commit("uiParams/dispatchAlert", {
             text: "No se pudo quitar del incidente",
             color: "primary",
             timeout: 4000
           });
 
-          this.loadingTable = false;
-        })
-        .finally(async () => {
-          this.loadingTable = false;
-        });
-
-
-
-  }
-},
+        })}
+  },
   computed: {
     ...mapGetters({
-      userInformation: "incident/incidentUserData"
+      userInformation: "incident/incidentUserData",
+      resourceIdIncidentId: "incident/incidentUserData"
 
     })
   }
